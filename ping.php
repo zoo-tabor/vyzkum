095878e51454f8d53f7db7138d78d34055f74248
Zoo Tabor <vyzkum@zootabor.eu>
2026-04-27 15:16:35 +0200

    Add isolated hosting ping test

---
+++ ping.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+header('Content-Type: text/plain; charset=utf-8');
+
+echo "pong\n";
+echo 'PHP ' . PHP_VERSION . "\n";
+echo 'SAPI ' . PHP_SAPI . "\n";
+echo '__DIR__ ' . __DIR__ . "\n";
