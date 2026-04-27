42cc7ee3ff0ecccdea69dbf86d2fe5a376ac779f
Zoo Tabor <vyzkum@zootabor.eu>
2026-04-27 15:46:26 +0200

    Fix FTP deploy root and remove diagnostics

--- ping-plain.php
+++
@@ -1,2 +0,0 @@
-<?php
-echo 'pong plain php';
--- ping.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-
-header('Content-Type: text/plain; charset=utf-8');
-
-echo "pong\n";
-echo 'PHP ' . PHP_VERSION . "\n";
-echo 'SAPI ' . PHP_SAPI . "\n";
-echo '__DIR__ ' . __DIR__ . "\n";
